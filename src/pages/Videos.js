073328e2dca96e7d9fc5f78270c8fa65c7ba5eb2
muma <GUW18@pitt.edu>
2022-12-20 23:42:31 -0500

    show videos in database

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,13 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import {Player} from "../components/player/Player";
+import axios from "axios";
 export const Videos = () => {
-    const [tmp_url, setTmpUrl] = useState("");
     const [url, setUrl] = useState("");
+    const [videoNameList, setVideoNameList] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost/api/video/getAllVideos")
+            .then((res)=>{
+              if(res.data !== ''){
+                  let videoList = res.data.map((video)=>{
+                      return video.name;
+                  })
+                  setVideoNameList(videoList);
+              }else{
+                  setVideoNameList([]);
+              }
+            })
+    },[]);
     return (
         <div>
             <Player videoName={url}/>
-            <input onChange={(evt)=>setTmpUrl(evt.target.value)}></input>
-            <button onClick={()=>setUrl(tmp_url)}>submit</button>
+            <ui>
+                {
+                    videoNameList.map((videoName,index)=>{
+                        return <li key={index} onClick={()=>setUrl(videoName)}>{index + "."}{videoName}</li>
+                    })
+                }
+            </ui>
+
         </div>
     );
 };
